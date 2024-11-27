5a16b0e44f6c19d3b3e1897870baede83fe63ffb
JaysTravels <zain@jaystravels.co.uk>
2024-11-27 13:33:25 +0000

    front end

--- components/footers/Front.Footer.js
+++ components/footers/Front.Footer.js
@@ -64,24 +64,16 @@ const FrontFooter = () => {
                     <div className="footer-logo">
                       <Image src={footerLogo} alt="" className="img-fluid" />
                     </div>
-                    <p>
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s, when an unknown
-                      printer took a galley of type and scrambled it to make a
-                      type specimen book. It has survived not only five
-                      centuries
-                    </p>
                     <ul className="contact-list">
                       <li>
                         <FontAwesomeIcon icon={faMapMarkerAlt} />
-                        A-32, Albany, Newyork.
+                           349 High Street, Smethwick, West Midlands, B66 3PB, United Kingdom
                       </li>
                       <li>
-                        <FontAwesomeIcon icon={faPhone} /> 518 - 457 - 5181
+                        <FontAwesomeIcon icon={faPhone}/> +44-8008101600
                       </li>
                       <li>
-                        <FontAwesomeIcon icon={faEnvelope} /> contact@gmail.com
+                        <FontAwesomeIcon icon={faEnvelope} />info@jaystravels.co.uk
                       </li>
                     </ul>
                   </div>
